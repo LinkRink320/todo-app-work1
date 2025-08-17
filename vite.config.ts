aa7dde6229bf65bae412b9cb80994ccac324edb9
LinkRink320 <shimooka.riku.0320@gmail.com>
2025-08-17 15:23:02 +0900

    GitHub ActionsでPagesデプロイ用ワークフローとvite baseパス追加

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/todo-app-work1/", // GitHub Pages用ベースパス
   plugins: [react()],
 });
